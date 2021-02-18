f32e8bf47a1dd1d444e9f2532b70f2454d3f3127
nanfang <2216812142@qq.com>
2021-02-19 00:16:09 +0800

    d

---
+++ User/build/generated/source/buildConfig/debug/com/example/user/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.user;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.example.user";
+  public static final String BUILD_TYPE = "debug";
+}
